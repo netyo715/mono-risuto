b66fd3a1957da6d9dd6e263ecc090b358567896c
sohei nishimura <nishimura715@gmail.com>
2025-08-24 21:19:12 +0900

    add: D1 database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const app = new Hono().use("*", cors({ origin: "*" })).get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+type Env = {
+  Bindings: {
+    DB: D1Database;
+  };
+};
+
+const app = new Hono<Env>()
+  .use("*", cors({ origin: "*" }))
+  .get("/", async (c) => {
+    const result = await c.env.DB.prepare("SELECT 1;").all();
+    console.log("result:", result.success);
+    return c.text("Hello Hono!");
+  });
 
 export type AppType = typeof app;
 
