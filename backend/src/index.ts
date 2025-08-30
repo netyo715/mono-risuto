16495cd07fcdc7db9ff69c0d9d88d091a46e4394
sohei nishimura <nishimura715@gmail.com>
2025-08-30 15:41:18 +0900

    add: github認証機能、drizzle

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import GitHub from "@auth/core/providers/github";
+import { authHandler, initAuthConfig, verifyAuth } from "@hono/auth-js";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
@@ -8,11 +10,47 @@ type Env = {
 };
 
 const app = new Hono<Env>()
-  .use("*", cors({ origin: "*" }))
+  .use(
+    "*",
+    cors({
+      origin: (_, c) => {
+        return c.env.CLIENT_URL;
+      },
+      credentials: true,
+    })
+  )
+  .use(
+    "*",
+    initAuthConfig((c) => ({
+      secret: c.env.AUTH_SECRET,
+      providers: [
+        GitHub({
+          clientId: c.env.GITHUB_CLIENT_ID,
+          clientSecret: c.env.GITHUB_CLIENT_SECRET,
+        }),
+      ],
+      callbacks: {
+        async redirect() {
+          return c.env.CLIENT_URL;
+        },
+        async signIn({ user }) {
+          // TODO ユーザー情報を読み込む、なかったら作る
+          return true;
+        },
+        async session({ session, token }) {
+          // TODO ユーザーIDをDBのものにする
+          return session;
+        },
+      },
+    }))
+  )
+  .use("/api/auth/*", authHandler())
+  .use("/api/*", verifyAuth())
   .get("/", async (c) => {
+    const auth = c.get("authUser");
     const result = await c.env.DB.prepare("SELECT 1;").all();
     console.log("result:", result.success);
-    return c.text("Hello Hono!");
+    return c.text(`Hello ${auth?.user?.name || "unknown user"}!`);
   });
 
 export type AppType = typeof app;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,37 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { hc } from "hono/client";
 import type { AppType } from "../../backend/src/index";
+import {
+  authConfigManager,
+  SessionProvider,
+  signIn,
+  useSession,
+} from "@hono/auth-js/react";
 
 const client = hc<AppType>(
   import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8787"
 );
 
+authConfigManager.setConfig({
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "http://127.0.0.1:8787",
+  credentials: "include",
+});
+
 function App() {
+  return (
+    <SessionProvider>
+      <Children />
+    </SessionProvider>
+  );
+}
+
+function Children() {
   const [text, setText] = useState("");
+  const session = useSession();
+
+  const handleSignIn = async () => {
+    signIn("github");
+  };
 
   const fetchData = async () => {
     const response = await client.index.$get();
@@ -33,6 +57,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>user: {session.data?.user?.name || "unknown user"}</p>
+      <button onClick={handleSignIn}>signIn</button>
       <div className="card">
         {text && <p>{text}</p>}
         <button onClick={fetchData}>fetch</button>
